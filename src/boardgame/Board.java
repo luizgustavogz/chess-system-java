27710417f7fc404352b667cbcab7e884a6015a10
Luiz Gustavo <luiz.gustavogz02@gmail.com>
2020-08-11 12:31:57 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -31,8 +31,13 @@ public class Board {
 	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
-	
+
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 }
--- src/chess/Color.java
+++ src/chess/Color.java
@@ -3,5 +3,5 @@ package chess;
 public enum Color {
 
 	BLACK,
-	WHTIE;
+	WHITE;
 }
